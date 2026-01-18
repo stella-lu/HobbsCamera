805269060f34be217a088889757c186950404f71
Stella Lu <stella-lu@outlook.com>
2026-01-18 11:46:58 -0800

    Initial Commit

---
+++ HobbsCamera/HobbsCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  HobbsCameraApp.swift
+//  HobbsCamera
+//
+//  Created by Stella Lu on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct HobbsCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HobbsCameraTests/HobbsCameraTests.swift
@@ -0,0 +1,17 @@
+//
+//  HobbsCameraTests.swift
+//  HobbsCameraTests
+//
+//  Created by Stella Lu on 1/18/26.
+//
+
+import Testing
+@testable import HobbsCamera
+
+struct HobbsCameraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
